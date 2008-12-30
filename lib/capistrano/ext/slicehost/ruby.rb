42a453c24182d084944961a44c01eca8842235e8
Fran Diéguez <fran.dieguez@mabishu.com>
2008-12-30 12:24:55 +0100

    updated ruby_enterprise_version to ruby-enterprise-1.8.6-20081215

--- lib/capistrano/ext/slicehost/ruby.rb
+++ lib/capistrano/ext/slicehost/ruby.rb
@@ -1,8 +1,9 @@
 # TODO: Automatically determine these values
-set :ruby_enterprise_version, "ruby-enterprise-1.8.6-20080810"
+set :ruby_enterprise_version, "ruby-enterprise-1.8.6-20081215"
 set :passenger_version, "2.0.3"
 
 namespace :ruby do
+
   desc "Install Ruby 1.8"
   task :setup_18, :roles => :app do
     sudo "aptitude install -y ruby1.8-dev ruby1.8 ri1.8 rdoc1.8 irb1.8 libreadline-ruby1.8 libruby1.8 libopenssl-ruby sqlite3 libsqlite3-ruby1.8"
@@ -26,7 +27,7 @@ namespace :ruby do
   desc "Install Phusion Passenger"
   task :install_passenger, :roles => :app do
     sudo "apt-get install apache2-mpm-prefork"
-    sudo "/opt/#{ruby_enterprise_version}/bin/ruby /opt/#{ruby_enterprise_version}/bin/gem install passenger"
+    sudo "/opt/#{ruby_enterprise_version}/bin/ruby /opt/#{ruby_enterprise_version}/bin/gem install passenger rake"
     sudo "/opt/#{ruby_enterprise_version}/bin/ruby /opt/#{ruby_enterprise_version}/bin/passenger-install-apache2-module"
 
     put render("passenger.load", binding), "/home/#{user}/passenger.load"
